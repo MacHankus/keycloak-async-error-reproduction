2346dc0262e634b3ae9e19afaa0a647402a787a6
Thomas Grainger <tagrain@gmail.com>
2024-10-10 11:58:37 +0100

    debug networking

--- src/infrastructure/auth/keycloak.py
+++ src/infrastructure/auth/keycloak.py
@@ -3,9 +3,13 @@ import contextlib
 from keycloak import KeycloakOpenID
 from settings import settings
 from keycloak import KeycloakAdmin
+import httpx
 
 @contextlib.asynccontextmanager
 async def create_keycloak_admin():
+    async with httpx.AsyncClient() as client:
+        await client.get("http://auth:8080/auth", timeout=1)
+        print(client.data)
     v = KeycloakAdmin(server_url=settings.KEYCLOAK_ADMIN_URL,
                         username=settings.KEYCLOAK_ADMIN_USERNAME,
                         password=settings.KEYCLOAK_ADMIN_PASSWORD,
